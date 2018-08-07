a4272bcc1137a2d4cb083ea932b8a60c3b7fed16
wanfan <446951428@qq.com>
2018-08-07 18:16:13 +0800

    modified pom.xml

---
+++ src/main/java/com/swjtu/configCentor/Application.java
@@ -0,0 +1,30 @@
+package com.swjtu.configCentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by wanfan01 on 2018/8/7.
+ */
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+    @RequestMapping("/")
+    public String index(){
+        return "hello, spring boot";
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+
+
+
+}
